bd683aca65ee3dff2ba0c1179c5cab8d9b406493
Paxton <paxton@drab.org>
2024-05-10 14:18:18 -0500

    remove blob follow

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 // BACKGROUND //
 const blob = document.getElementById("blob");
 
-window.onpointermove = event => {
-  const { clientX, clientY } = event;
+// window.onpointermove = event => {
+//   const { clientX, clientY } = event;
 
-  blob.animate({
-    left: `${clientX}px`,
-    top: `${clientY}px`
-  }, { duration: 1000, fill: "forwards"});
-}
+//   blob.animate({
+//     left: `${clientX}px`,
+//     top: `${clientY}px`
+//   }, { duration: 1000, fill: "forwards"});
+// }
 
 // BUTTONS //
 const docs_page = document.querySelector('.docs');
 
 function  open_docs() {
   docs_page.style.left = '0%';
+}
+
+function open_main() {
+  docs_page.style.left = '100%';
 }
