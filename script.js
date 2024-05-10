d568bbc9db90816d10a0932f44f3fd3aed71ec20
Paxton <paxton@drab.org>
2024-05-10 13:40:00 -0500

    add docs screen

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// BACKGROUND //
 const blob = document.getElementById("blob");
 
 window.onpointermove = event => {
@@ -6,5 +7,20 @@ window.onpointermove = event => {
   blob.animate({
     left: `${clientX}px`,
     top: `${clientY}px`
-  }, { duration: 3000, fill: "forwards"});
+  }, { duration: 1000, fill: "forwards"});
+}
+
+// BUTTONS //
+const docs_page = document.querySelector('.docs');
+let toggled_docs = false;
+
+function toggle_docs() {
+  if (toggled_docs) {
+    docs_page.style.left = '100%';
+    toggled_docs = false;
+  }
+  else {
+    docs_page.style.left = '0%';
+    toggled_docs = true;
+  }
 }
